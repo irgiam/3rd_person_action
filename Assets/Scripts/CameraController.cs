aaa7abb262d10041494593ba31f0df69c67ce5c6
irgiam <imuqsit@gmail.com>
2020-08-27 16:13:52 +0700

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed = 10;
+    public Transform target, player;
+    float mouseX, mouseY;
+
+    private void Start()
+    {
+        //Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 40);
+
+        transform.LookAt(target);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        } else
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 10f;
+    public float gravity = 20f;
+    Vector3 moveDirection = Vector3.zero;
+    float horizontalMove = 0;
+    float verticalMove = 0;
+    float currentSpeed;
+    public GameObject center;
+    CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal");
+            verticalMove = Input.GetAxisRaw("Vertical");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.Rotate(0, horizontalMove, 0);
+        moveDirection = new Vector3(0, 0, verticalMove);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= walkSpeed;
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //transform.RotateAround(center.transform.position, Vector3.up*verticalMove, walkSpeed * Time.deltaTime); //jgn lupa verticalmove diluar grounded
+    }
+}
