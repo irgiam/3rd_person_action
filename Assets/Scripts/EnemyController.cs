8e04022162a516b83864a9bb0657136036e73cce
irgiam <imuqsit@gmail.com>
2020-09-07 17:34:32 +0700

    add healthbar
    add helathbar for player and enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float health;
+
+    public GameObject canvas;
+    public Slider healthBar;
+    public Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetHealth();
+    }
+
+    private void LateUpdate()
+    {
+        canvas.transform.LookAt(canvas.transform.position + cam.forward);
+    }
+
+    public void TakenDamage(float damage)
+    {
+        health -= damage;
+        if (health < 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+        healthBar.value = health;
+    }
+
+    void SetHealth()
+    {
+        health = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = health;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,10 +20,20 @@ public class PlayerController : MonoBehaviour
     bool isGrounded;
     Vector3 velocity = new Vector3();
 
+    float attackRate = 2f;
+    float nextAttackTime = 0f;
+    public Transform attackPoint;
+    public float attackDamage = 30f;
+
     private void Awake()
     {
         controller = this.GetComponent<CharacterController>();
         thisaAnimator = this.GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        SetHealth();
     }
 
     // Update is called once per frame
@@ -30,6 +41,7 @@ public class PlayerController : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
+
         isGrounded = Physics.CheckSphere(this.transform.position, 0.1f, groundLayer);
 
         if (Input.GetKey(KeyCode.LeftShift))
@@ -43,17 +55,23 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
+            TakeDamage(20f);
         }
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && isGrounded && Time.time >= nextAttackTime)
         {
             Attack();
+            nextAttackTime = Time.time + 2f / attackRate;
         }
 
         float movement = Mathf.Clamp((Mathf.Abs(horizontalMove) + Mathf.Abs(verticalMove)), 0, 1);
         thisaAnimator.SetFloat("Speed", (movement * moveSpeed));
         thisaAnimator.SetBool("IsJumping", !isGrounded);
-        HandleMovement();
+        if (Time.time >= nextAttackTime)
+        {
+            HandleMovement();
+        }
+        
         GravityHandler();
     }
 
@@ -91,8 +109,38 @@ public class PlayerController : MonoBehaviour
         velocity.y += Mathf.Sqrt(jumpHeight * 3.0f * gravity);
     }
 
+    public LayerMask enemyLayer;
     void Attack()
     {
-        Debug.Log("Atttack!");
+        thisaAnimator.SetTrigger("Attack");
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, 0.8f, enemyLayer);
+        foreach (Collider enemy in hitEnemies)
+        {
+            //Debug.Log("we hit" + enemy.name);
+            enemy.GetComponent<EnemyController>().TakenDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawSphere(attackPoint.position, 0.8f);
+    }
+
+
+    public Slider healthBar;
+    public float maxHealth = 100f;
+    float health;
+
+    void SetHealth()
+    {
+        health = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = health;
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        healthBar.value = health;
     }
 }
