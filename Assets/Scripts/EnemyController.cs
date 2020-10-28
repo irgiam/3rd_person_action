143e7dc8170ae6d836f8a84965cd67f256092cfa
irgiam <imuqsit@gmail.com>
2020-10-28 22:14:19 +0700

    update unity 2019.4.13

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,21 +5,51 @@ using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour
 {
+    CharacterController controller;
     public float maxHealth = 100;
     public float health;
+    public float gravity = 20f;
 
     public GameObject canvas;
     public Slider healthBar;
     public Transform cam;
+    public LayerMask groundLayer;
+    Vector3 velocity = new Vector3();
+    bool isGrounded;
+
+    private void Awake()
+    {
+        controller = this.GetComponent<CharacterController>();
+    }
     // Start is called before the first frame update
     void Start()
     {
         SetHealth();
     }
 
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(this.transform.position, 0.1f, groundLayer);
+        GravityHandler();
+    }
+
     private void LateUpdate()
     {
         canvas.transform.LookAt(canvas.transform.position + cam.forward);
+    }
+
+    void GravityHandler()
+    {
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity = Vector3.zero;
+            //Debug.Log("Grounded!!");
+        }
+        else
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        controller.Move(velocity * Time.deltaTime);
     }
 
     public void TakenDamage(float damage)
